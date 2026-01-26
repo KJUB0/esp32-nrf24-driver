99b1b75ff66eac09da3d85dc4c429ac8d2fe0534
KJUB0 <jakubferencik3@gmail.com>
2026-01-26 21:29:14 +0100

    modified CMakeLists for cleaner github looks and added initialization of pins and configuration

--- main/main.c
+++ main/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "driver/spi_master.h"
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #define PIN_MISO 19 //data from radio to esp
 #define PIN_MOSI 23 //data from esp to radio
 #define PIN_CLK 18 //clock
+#define PIN_CSN 5
+#define PIN_CE 4
 
-void app_main(void)
-{
-    printf("Inicializacia busov");
+spi_bus_config_t bus_configuration() {
+    printf("Inicializacia busov\n");
 
     //bus inicialization
-    spi_bus_config_t busconfig= {
+    spi_bus_config_t config= {
         .miso_io_num = PIN_MISO,
         .mosi_io_num = PIN_MOSI,
         .sclk_io_num = PIN_CLK,
@@ -25,15 +28,41 @@ void app_main(void)
         //limit for transactions
         .max_transfer_sz = 32 //bytes
     };
+    return config;
+}
 
+void app_main(void)
+{
+    
+    spi_bus_config_t bus_cfg = bus_configuration();
 
-    esp_err_t status_message = spi_bus_initialize(SPI2_HOST, &busconfig, SPI_DMA_CH_AUTO);
+    esp_err_t status_message = spi_bus_initialize(SPI2_HOST, &bus_cfg, SPI_DMA_CH_AUTO);
 
     if (status_message == ESP_OK) {
         printf("[SUCCESS] SPI Bus is active on pins 18, 19, 23.\n");
+
+        spi_device_interface_config_t dev_config = {
+            .mode = 0,                     //NRF24 works in Mode 0 (Clock Low, Phase 1 edge)
+            .clock_speed_hz = 4000000,     //4 MHz (max is 10MHz)
+            .spics_io_num = PIN_CSN,       //csn pin
+            .queue_size = 7,               
+        };
+
+        spi_device_handle_t radio; //inititalization nrf card
+        esp_err_t status_message_nrf = spi_bus_add_device(SPI2_HOST, &dev_config, &radio);
+
+        if (status_message_nrf == ESP_OK) {
+            printf("NRF24 Device successfully added to the bus!\n");
+        } else {
+            printf("Failed to add NRF24 device. Error: %s\n", esp_err_to_name(status_message_nrf));
+        }
     } else {
         printf("[FAIL] Could not init SPI. Error code: %d\n", status_message);
     }
+
+    //ce pin as output
+    gpio_set_direction(PIN_CE, GPIO_MODE_OUTPUT);
+    gpio_set_level(PIN_CE, 0); // Start with radio disabled (Low)
 
     //continuous loop to stop the esp from sleeping
     while(1) {
